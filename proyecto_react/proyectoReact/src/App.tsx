3426bcc214b7e337ed88ffe6bccbdff7db5700e7
Marcos <marcosdimecom@gmail.com>
2024-10-03 23:56:52 -0300

    Proyecto React

---
+++ proyecto_react/proyectoReact/src/App.tsx
@@ -0,0 +1,12 @@
+import { AppProduct } from "./components/AddProduct/AppProduct"
+
+export const App = () => {
+    
+    return (
+        <div style={{display:'flex', flexDirection:"column"}}>
+
+            <AppProduct></AppProduct>
+        </div>
+    
+    )
+}
---
+++ proyecto_react/proyectoReact/src/components/ComponentUseEffect/ComponentUseEffect.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+
+    
+
+export const ComponentUseEffect = () => {
+    const [state,setState] = useState(false)
+
+    useEffect(()=>{
+        console.log("Componenete montado");
+        return ()=>{
+            console.log("Componente desmontado");
+        }
+    })
+    
+    useEffect(()=>{
+        console.log(state);
+    },[state])
+
+
+    return (
+    <div>
+        <p>{state ? "Es true": "Es false"}</p>
+        <button
+        onClick={()=>{
+            setState(!state)
+        }}
+        >
+            Cambiar state
+        </button>
+    </div>
+    )
+}
